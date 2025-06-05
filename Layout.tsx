a18560ae020ccb1b5f92f3ebb9142f28092349ac
Choxime <maxgrelet45@gmail.com>
2025-06-05 12:16:34 +0200

    Add accessibility skip link and refine Tailwind config

--- Layout.tsx
+++ Layout.tsx
@@ -13,8 +13,12 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <ScrollProgress />
+      <a href="#main-content" className="skip-link">
+        Skip to content
+      </a>
       <Header />
       <motion.main
+        id="main-content"
         className="flex-grow"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
